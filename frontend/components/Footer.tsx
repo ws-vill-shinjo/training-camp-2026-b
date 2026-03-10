fce2dadb6a6b754b6bbc08940800c631eec8d867
nandemoyokatta <nanndemoyokatta@gmail.com>
2026-03-10 16:03:30 +0000

    add footer

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -2,6 +2,10 @@ import { JSX } from "react";
 
 export const Footer = (): JSX.Element => {
   return (
-    <div className="fixed bottom-0 bg-[#E8A87C] h-30 w-screen flex items-center justify-center"></div>
+    <div className="fixed bottom-0 bg-[#E8A87C] h-30 w-screen flex items-center justify-around">
+      <div className="w-88 h-88 bg-white"></div>
+      <div className="w-88 h-88 bg-white"></div>
+      <div className="w-88 h-88 bg-white"></div>
+    </div>
   );
 };
