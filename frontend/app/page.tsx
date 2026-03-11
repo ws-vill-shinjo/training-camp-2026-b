5528079abb0c4d9aa8afad966ea699db2dad48a7
nandemoyokatta <nanndemoyokatta@gmail.com>
2026-03-11 05:58:33 +0000

    add_appgrade

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,12 +1,18 @@
 "use client";
 import { Header } from "@/components/Header";
+import { Upgradecard } from "@/components/Upgrade_card";
 
 export default function Home() {
   return (
-    <div>
-      <Header>
-        <h1 className="text-4xl">ホーム</h1>
-      </Header>
-    </div>
+    <>
+      <div>
+        <Header>
+          <h1 className="text-4xl">ホーム</h1>
+        </Header>
+      </div>
+      <div>
+        <Upgradecard label="じゃがいも" lv={1} />
+      </div>
+    </>
   );
 }
