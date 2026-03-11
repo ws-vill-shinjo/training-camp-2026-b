3812c13f3372fd28aea56165e4c6571c023338bb
nandemoyokatta <nanndemoyokatta@gmail.com>
2026-03-11 01:43:37 +0000

    add_footer

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -1,18 +1,12 @@
 "use client";
 import { JSX } from "react";
-import Image from "next/image";
+import { Footer_icon } from "./Footer_icon";
 export const Footer = (): JSX.Element => {
   return (
     <div className="fixed bottom-0 bg-[#E8A87C] h-30 w-screen flex items-center z--1 justify-around">
-      <div className="rounded-full w=80 h=80">
-        <Image src="/house.png" alt="アイコン" width={78} height={78} />
-      </div>
-      <div className="rounded-full w=80 h=80">
-        <Image src="/arrow-up.png" alt="アイコン" width={78} height={78} />
-      </div>
-      <div className="rounded-full w=80 h=80">
-        <Image src="/book-search.png" alt="アイコン" width={78} height={78} />
-      </div>
+      <Footer_icon image="/arrow-up.png" />
+      <Footer_icon image="/house.png" />
+      <Footer_icon image="/book-search.png" />
     </div>
   );
 };
